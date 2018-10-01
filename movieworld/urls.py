c4d28a3b88bb664f73fa7f6787935c067205f173
kevinliang1976 <kevinliang188@hotmail.com>
2018-10-01 19:31:41 +1000

    update urls

--- movieworld/urls.py
+++ movieworld/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from movie import views
+
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^movie/', include('movie.urls')),
     url(r'^admin/', admin.site.urls),
 ]
